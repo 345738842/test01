d3234caafc4305c8bf55223553308ca30085db95
hu_ke； <345738842@qq.com>
2019-08-26 14:34:43 +0800

    test

--- src/main/java/com/ydw/subsystem/config/Swagger2Config.java
+++ src/main/java/com/ydw/subsystem/config/Swagger2Config.java
@@ -1,6 +1,4 @@
 package com.ydw.subsystem.config;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +10,6 @@ import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import java.util.ArrayList;
 import java.util.List;
 
